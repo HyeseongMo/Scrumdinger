7473135bc703289e59583b4409a69e0c6fb0436e
HyeseongMo <Hyeseong0914@naver.com>
2023-10-25 00:33:28 +0900

    ScrumTimer Class 추가
    - Managing state and life cycle (section 4)
    - ScrumTimer 클래스는 회의 상태를 주기적으로 업데이트합니다. 타이머는 스피커가 변경될 때 speakerChangedAction 클로저를 호출하고, @Published 속성을 통해 다른 중요한 상태 변경 사항을 공유합니다.

---
+++ Scrumdinger/Models/ScrumTimer.swift
@@ -0,0 +1,109 @@
+import Foundation
+
+@MainActor
+final class ScrumTimer: ObservableObject {
+    struct Speaker: Identifiable {
+        let name: String
+        var isCompleted: Bool
+        let id = UUID()
+    }
+    
+    @Published var activeSpeaker = ""
+    @Published var secondsElapsed = 0
+    @Published var secondsRemaining = 0
+    
+    private(set) var speakers: [Speaker] = []
+    private(set) var lengthMinutes: Int
+    
+    var speakerChangedAction: (() -> Void)?
+    
+    private weak var timer: Timer?
+    private var timerStopped = false
+    private var frequency: TimeInterval {1.0 / 60.0}
+    private var lengthInSeconds: Int { lengthMinutes * 60 }
+    private var secondsPerSpeaker: Int {
+        (lengthMinutes * 60) / speakers.count
+    }
+    private var secondsElapsedForSpeacker: Int = 0
+    private var speakerIndex: Int = 0
+    private var speakerText: String {
+        return "Speaker \(speakerIndex + 1): \(speakers[speakerIndex].name)"
+    }
+    private var startDate: Date?
+    
+    init(lengthInMinutes: Int = 0, attendees: [DailyScrum.Attendee] = []) {
+        self.lengthMinutes = lengthInMinutes
+        self.speakers = attendees.speakers
+        secondsRemaining = lengthInSeconds
+        activeSpeaker = speakerText
+    }
+    
+    func startScrum() {
+        timer = Timer.scheduledTimer(withTimeInterval: frequency, repeats: true) {
+            [weak self]  timer in self?.update()
+        }
+        timer?.tolerance = 0.1
+        changeToSpeaker(at: 0)
+    }
+    
+    func stopScrum () {
+        timer?.invalidate()
+        timerStopped = true
+    }
+    
+    nonisolated func skipSpeakeer() {
+        Task { @MainActor in
+            changeToSpeaker(at: speakerIndex + 1)
+        }
+    }
+    
+    private func changeToSpeaker(at index: Int) {
+        if (index > 0) {
+            let previousSpeakerIndex = index - 1
+            speakers[previousSpeakerIndex].isCompleted = true
+        }
+        secondsElapsedForSpeacker = 0
+        guard index < speakers.count else { return }
+        speakerIndex = index
+        activeSpeaker = speakerText
+        
+        secondsElapsed = index * secondsPerSpeaker
+        secondsRemaining = lengthInSeconds - secondsElapsed
+        startDate = Date()
+    }
+    
+    nonisolated private func update() {
+        
+        Task { @MainActor in
+            guard let startDate, !timerStopped else { return }
+            let secondsElapsed = Int(Date().timeIntervalSince1970 - startDate.timeIntervalSince1970)
+            secondsElapsedForSpeacker = secondsElapsed
+            self.secondsElapsed = secondsPerSpeaker * speakerIndex + secondsElapsedForSpeacker
+            guard secondsElapsed <= secondsPerSpeaker else {return}
+            secondsRemaining = max(lengthInSeconds - self.secondsElapsed, 0)
+            
+            if secondsElapsedForSpeacker >= secondsPerSpeaker {
+                changeToSpeaker(at: speakerIndex + 1)
+                speakerChangedAction?()
+            }
+        }
+        
+    }
+    
+    func reset(lengInMinutes: Int, attendees: [DailyScrum.Attendee]) {
+        self.lengthMinutes = lengInMinutes
+        self.speakers = attendees.speakers
+        secondsRemaining = lengthInSeconds
+        activeSpeaker = speakerText
+    }
+}
+
+extension Array<DailyScrum.Attendee> {
+    var speakers: [ScrumTimer.Speaker] {
+        if isEmpty {
+            return [ScrumTimer.Speaker(name: "Speaker 1", isCompleted: false)]
+        } else {
+            return map { ScrumTimer.Speaker(name: $0.name, isCompleted: false)}
+        }
+    }
+}
