2697f4e57253011fbe47f0f657394cbd43916136
HyeseongMo <Hyeseong0914@naver.com>
2023-10-27 00:03:44 +0900

    Meeting 화면에 MeetingFooterView 적용

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -10,26 +10,33 @@ import SwiftData
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16.0).fill(scrum.theme.mainColor)
             VStack {
-                
-                
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Spacer()
-                Circle().stroke(lineWidth: 24)
+                Circle()
+                    .stroke(lineWidth: 24)
+                    .padding()
                 Spacer()
-                HStack {
-                    Text("Speaker 1 of 3")
-                    Spacer()
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Image(systemName: "forward.fill")
-                    }
-                    .accessibilityLabel("Next speaker")
-                }
+            
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeakeer)
+                
             }
-            .padding()
-        }.padding().foregroundColor(scrum.theme.accentColor).navigationBarTitleDisplayMode(.inline)
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
